5ec146ae58beb4d2eed855a0754cd3c3a25269e4
HemantGTX950 <hem.ban.061@gmail.com>
2018-05-14 15:03:59 +0530

    First Commit

--- app/src/main/java/com/manan/mchat/activities/MainActivity.java
+++ app/src/main/java/com/manan/mchat/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.manan.mchat;
+package com.manan.mchat.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.manan.mchat.R;
 
 public class MainActivity extends AppCompatActivity {
 
